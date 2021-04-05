198bd74f59f662631ad269d74689dbfd441858c7
Jerrika Barnett <jerrikabarnett@gmail.com>
2021-04-05 18:59:31 -0500

    Update urllib-script.py

--- urllib-script.py
+++ urllib-script.py
@@ -12,15 +12,15 @@ url = 'https://genius.com/Demi-lovato-met-him-last-night-lyrics'  # url to visit
 values = {'s':'basic', 
           'submit':'search'}  # dictionary used
 
-data = urllib.parse.urlencode(values)       #encode values
-data = data.encode('utf-8')                 # type of coding
-req = urllib.request.Request(url,data)      #encode data
-resp = urllib.request.urlopen(req)          #request
+data = urllib.parse.urlencode(values)  #encode values
+data = data.encode('utf-8')   # type of coding
+req = urllib.request.Request(url,data)   #encode data
+resp = urllib.request.urlopen(req)   #request
 respData = resp.read()                      
 
 #print(respData)
 
-paragraphs = re.findall(r'<br>(.*?)</br>',str(respData)) #inbetween paranthesis is content we're looking for iven is everything between paragragh tags
-
-for eachbr in paragraphs:
-    print (eachbr)
+paragraphs = re.findall(r'<br>(.*?)</br>',str(respData)) #() is content we're looking for to identify
+ 
+for eachP in paragraphs:
+    print (eachP)
