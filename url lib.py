120829f641827f6577a8950f070bc71ccfb9379b
Jerrika Barnett <jerrikabarnett@gmail.com>
2021-04-05 18:56:47 -0500

    Add files via upload

---
+++ url lib.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 17:10:03 2021
+
+@author: Jerrika
+"""
+import urllib.request
+import urllib.parse #values to post request
+import re
+
+url = 'https://genius.com/Demi-lovato-met-him-last-night-lyrics'  # url to visit
+values = {'s':'basic', 
+          'submit':'search'}  # dictionary used
+
+data = urllib.parse.urlencode(values)       #encode values
+data = data.encode('utf-8')                 # type of coding
+req = urllib.request.Request(url,data)      #encode data
+resp = urllib.request.urlopen(req)          #request
+respData = resp.read()                      
+
+#print(respData)
+
+paragraphs = re.findall(r'<br>(.*?)</br>',str(respData)) #inbetween paranthesis is content we're looking for iven is everything between paragragh tags
+
+for eachbr in paragraphs:
+    print (eachbr)
